10b437d75b356efdac54ecb46f5be2c3c62b1694
zhouwen <136600197@qq.com>
2018-04-11 23:00:33 +0800

    首页逻辑完成一半

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -16,4 +16,20 @@ $(function () {
      })
 
 
+     //二级菜单的显示与隐藏;
+     $('.second').prev().on('click',function () {
+         $(this).next().slideToggle()
+       })
+
+    
+       //给icon_1 注册点击事件;
+    $('.icon_1').on('click',function () { 
+
+        //让侧边栏隐藏
+        $('.lt_aside').toggleClass('now')
+        //让main的padding-left 变为0;
+        $('.lt_main').toggleClass('now')
+     })
+
+
 })
