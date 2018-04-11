ff3b544271a2a0427ecaec84d97818d11f3a7b39
zhouwen <136600197@qq.com>
2018-04-11 13:48:11 +0800

    登录页面逻辑复习完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,96 +1,88 @@
 $(function () {
 
-    //1.校验表单
+    //1 校验表单
     $('form').bootstrapValidator({
+        fields:{
 
-        //要求 用户名不能为空
-        //      密码不能为空  长度在6-12位
-        //配置校验规则
-        fields: {
-            username: {
-                validators: {
+            username:{
+                validators:{
                     //不能为空
-                    notEmpty: {
-                        message: '用户名不能为空'
+                    notEmpty:{
+                        message:'用户名不能为空'
                     },
 
-                    //长度校验
                     stringLength: {
-                        min: 2,
-                        max: 6,
-                        message: '用户名长度在2-6位'
+                        min:2,
+                        max:12,
+                        message:'用户名在2-12位之间'
                     },
-                    //专门用来提示
-                    callback :{
-                        message:'用户名错误'
+                    callback:{
+                        message:'用户名不存在'
                     }
-                },
-
+                }
             },
 
-            password: {
-                validators: {
-                    notEmpty: {
-                        message: '密码不能为空'
+            password:{
+                validators:{
+                    //不能为空
+                    notEmpty:{
+                        message:'密码不能为空'
                     },
 
-                    //长度校验
                     stringLength: {
-                        min: 6,
-                        max: 12,
-                        message: '密码长度在6-12位'
+                        min:6,
+                        max:12,
+                        message:'密码在6-12位之间'
                     },
-                     //专门用来提示
-                     callback :{
+                    callback:{
                         message:'密码错误'
                     }
                 }
             }
+
+
         },
 
-        //配置小图标
         feedbackIcons: {
             valid: 'glyphicon glyphicon-ok',
             invalid: 'glyphicon glyphicon-remove',
             validating: 'glyphicon glyphicon-refresh'
           },
-        
     })
 
-    //2.点击登录阻止默认行为,发送ajax请求 正确跳转到主页 
+
+    //2 阻止默认时间,发送ajax请求 成功跳转页面
     $('form').on('success.form.bv',function (e) { 
-        //组织浏览器默认行为
+        //阻止浏览器默认时间
         e.preventDefault();
-        //发送请求
+        
+        //发送ajax请求
         $.ajax({
             type:'post',
             url:'/employee/employeeLogin',
             data:$('form').serialize(),
-            datatype:'json',
             success:function (info) { 
                 if(info.error===1000){
-                    $("form").data('bootstrapValidator').updateStatus('username','INVALID','callback')
+                   $('form').data('bootstrapValidator').updateStatus('username','INVALID','callback')
                 }
 
                 if(info.error===1001){
-                    $("form").data('bootstrapValidator').updateStatus('password','INVALID','callback')
+                    $('form').data('bootstrapValidator').updateStatus('password','INVALID','callback')
                 }
 
                 if(info.success){
-                    //成功跳转
-                    location.href ="index.html"
+                   location.href="index.html"
                 }
              }
         })
      })
 
 
-     //3.重置表单  清除所有样式
-
+     //3 重置表单 
      $("[type='reset']").on('click',function () { 
-
-        $("form").data('bootstrapValidator').resetForm();
+        $('form').data('bootstrapValidator').resetForm()
       })
+    
 
 
-});
+  })
